7c420384a1e54aaef94ec1c21f19fbf526813046
yxcindy <yixincindy19@gmail.com>
2018-07-22 14:40:02 -0700

    initial scripts

---
+++ Assets/Scripts/ArrowPressure.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPressure : MonoBehaviour {
+	public GameObject door;
+	//private TouchArrow arrow = GetComponent<TouchArrow>();
+	bool touched = TouchArrow.touched;
+	// Update is called once per frame
+	void Update () {
+		if(touched){
+			disappear();
+		}else{
+			appear();
+		}
+	}
+
+	void disappear(){
+		door.SetActive(false);
+	}
+
+	void appear(){
+		door.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/TouchArrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.String.Equals;
+
+public class TouchArrow : MonoBehaviour {
+	public static bool touched = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision other) 
+    {
+		if(String.Equals(other.gameObject.name, "Arrow") == true){
+			touched = true;
+		}
+
+    }
+
+	void onCollisionExit (Collision other)
+	{
+		touched = false;
+	}
+}
